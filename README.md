59c87c98f8f1d1fa58c5c08eb8fe89e8be8bec42
Luiza Carvalho <luiza177music@gmail.com>
2021-02-16 15:14:31 +0100

    starting point

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
-# wx_cmake_template
-
-A template for multiplatform WxWidgets projects using CMake
-
-For details see the blog post: https://justdevtutorials.medium.com/wxwidgets-cmake-multiplatform-superbuild-4ea86c4e6eda
-
-Also check out the video tutorial showcasing installation on Linux, Windows and Mac OS X: https://www.youtube.com/watch?v=MfuBS9n5_aY&t=4s
+# optimized_m4a_converter
--- src/main.cpp
+++ src/main.cpp
@@ -1,68 +1,172 @@
-// wxWidgets "Hello world" Program
 // For compilers that support precompilation, includes "wx/wx.h".
 #include <wx/wxprec.h>
 #ifndef WX_PRECOMP
 #include <wx/wx.h>
 #endif
-class MyApp : public wxApp
+
+#include <wx/dnd.h>
+#include <wx/filedlg.h>
+#include <wx/listctrl.h>
+#include <algorithm>
+
+class App : public wxApp
 {
 public:
     virtual bool OnInit();
 };
-class MyFrame : public wxFrame
+
+class FrameMain : public wxFrame //, wxFileDropTarget
 {
 public:
-    MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size);
+    FrameMain(const wxString &title, const wxPoint &pos, const wxSize &size);
+    //virtual bool OnDropFiles(wxCoord x, wxCoord y, const wxArrayString &filenames);
 
 private:
-    void OnHello(wxCommandEvent &event);
+    void OnConvert(wxCommandEvent &event);
+    void OnOpen(wxCommandEvent &event);
+    void OnClear(wxCommandEvent &event);
     void OnExit(wxCommandEvent &event);
     void OnAbout(wxCommandEvent &event);
+
     wxDECLARE_EVENT_TABLE();
 };
+
 enum
 {
-    ID_Hello = 1
+    ID_Convert = 1,
+    ID_Clear
 };
-wxBEGIN_EVENT_TABLE(MyFrame, wxFrame)
-    EVT_MENU(ID_Hello, MyFrame::OnHello)
-        EVT_MENU(wxID_EXIT, MyFrame::OnExit)
-            EVT_MENU(wxID_ABOUT, MyFrame::OnAbout)
-                wxEND_EVENT_TABLE()
-                    wxIMPLEMENT_APP(MyApp);
-bool MyApp::OnInit()
+
+// clang-format off
+wxBEGIN_EVENT_TABLE(FrameMain, wxFrame)
+    EVT_MENU(ID_Convert, FrameMain::OnConvert)
+    EVT_MENU(wxID_EXIT, FrameMain::OnExit)
+    EVT_MENU(wxID_ABOUT, FrameMain::OnAbout)
+    EVT_MENU(wxID_OPEN, FrameMain::OnOpen)
+    EVT_BUTTON(ID_Convert, FrameMain::OnConvert)
+    EVT_BUTTON(ID_Clear, FrameMain::OnClear)
+wxEND_EVENT_TABLE()
+wxIMPLEMENT_APP(App);
+// clang-format on
+
+bool App::OnInit()
 {
-    MyFrame *frame = new MyFrame("Hello World", wxPoint(50, 50), wxSize(450, 340));
+    FrameMain *frame = new FrameMain(_("Optimized M4A Converter"), wxPoint(50, 50), wxSize(500, 200));
     frame->Show(true);
     return true;
 }
-MyFrame::MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size)
-    : wxFrame(NULL, wxID_ANY, title, pos, size)
+
+FrameMain::FrameMain(const wxString &title, const wxPoint &pos, const wxSize &size)
+    : wxFrame(nullptr, wxID_ANY, title, pos, size)
 {
+    SetMinSize(wxSize(150, 100));
+
+    // menu bar [wxMenuBar, wxMenu, (wxMenuItem)]
     wxMenu *menuFile = new wxMenu;
-    menuFile->Append(ID_Hello, "&Hello...\tCtrl-H",
-                     "Help string shown in status bar for this menu item");
+    menuFile->Append(ID_Convert, _("&Convert...\tCtrl-Shift-C"), _("Convert shown files")); // "\t" tells a shortcut will follow
+    menuFile->Append(wxID_OPEN, _("&Open...\tCtrl-O"), _("Select files to be converted"));
     menuFile->AppendSeparator();
-    menuFile->Append(wxID_EXIT);
+    menuFile->Append(wxID_EXIT); // doesn't have default shortcut on Windows
+
     wxMenu *menuHelp = new wxMenu;
     menuHelp->Append(wxID_ABOUT);
+
     wxMenuBar *menuBar = new wxMenuBar;
-    menuBar->Append(menuFile, "&File");
-    menuBar->Append(menuHelp, "&Help");
+    menuBar->Append(menuFile, _("&File")); // "&" turns "F" into an 'accelerator', for Windows' Alt + letter
+    menuBar->Append(menuHelp, _("&Help"));
     SetMenuBar(menuBar);
+
+    // main panel / sizers --- KEEP?
+    wxPanel *panelMain = new wxPanel(this, wxID_ANY);
+    panelMain->SetBackgroundColour("black");
+
+    // buttons
+    wxButton *buttonConvert = new wxButton(panelMain, ID_Convert, _("Convert"), wxDefaultPosition, wxSize(100, 20));
+    buttonConvert->SetDefault();
+    wxButton *buttonClear = new wxButton(panelMain, ID_Clear, _("Clear"), wxDefaultPosition, wxSize(50, 20));
+
+    wxBoxSizer *sizerHorziontalButtons = new wxBoxSizer(wxHORIZONTAL);
+    sizerHorziontalButtons->AddStretchSpacer();
+    sizerHorziontalButtons->Add(buttonClear, 0, wxLEFT | wxTOP | wxBOTTOM | wxRIGHT, 10);
+    sizerHorziontalButtons->Add(buttonConvert, 0, wxTOP | wxBOTTOM | wxRIGHT, 10);
+
+    // list
+    wxListCtrl *listCtrl = new wxListCtrl(panelMain, wxID_ANY, wxDefaultPosition, wxSize(500, 300), wxLC_REPORT);
+    listCtrl->AppendColumn(_("File"), wxLIST_FORMAT_LEFT, 300);
+    listCtrl->AppendColumn(_("Status"), wxLIST_FORMAT_CENTER, 100);
+
+    // main sizer
+    wxBoxSizer *sizerVerticalMain = new wxBoxSizer(wxVERTICAL);
+    sizerVerticalMain->Add(listCtrl, 1, wxEXPAND);
+    sizerVerticalMain->Add(sizerHorziontalButtons, 1, wxEXPAND);
+    panelMain->SetSizerAndFit(sizerVerticalMain);
+
+    // drag-n-drop
+    //SetDropTarget(this);
+
+    // status bar
     CreateStatusBar();
-    SetStatusText("Welcome to wxWidgets!");
+    SetStatusText("0 files");
 }
-void MyFrame::OnExit(wxCommandEvent &event)
+
+void FrameMain::OnExit(wxCommandEvent &event)
 {
     Close(true);
 }
-void MyFrame::OnAbout(wxCommandEvent &event)
+
+void FrameMain::OnAbout(wxCommandEvent &event)
 {
-    wxMessageBox("This is a wxWidgets' Hello world sample",
-                 "About Hello World", wxOK | wxICON_INFORMATION);
+    wxMessageBox(_("This is a WAV to AAC converter that produces optimized M4A/AAC files"),
+                 _("About Optimized M4A Converter"),
+                 wxOK | wxICON_INFORMATION);
 }
-void MyFrame::OnHello(wxCommandEvent &event)
+
+void FrameMain::OnConvert(wxCommandEvent &event)
 {
-    wxLogMessage("Hello world from wxWidgets!");
+    PushStatusText(_("DONE!"));
+    // wxSleep(5);
+    // PopStatusText();
 }
+
+void FrameMain::OnClear(wxCommandEvent &event)
+{
+    wxLogMessage("Clear!");
+}
+
+void FrameMain::OnOpen(wxCommandEvent &event)
+{
+    wxFileDialog openFileDialog(this,
+                                _("Select files to be converted"),
+                                "",
+                                "",
+                                "WAV or M4A files (*.wav; *.m4a)|*.wav;*.m4a",
+                                wxFD_OPEN | wxFD_FILE_MUST_EXIST | wxFD_MULTIPLE);
+
+    if (openFileDialog.ShowModal() == wxID_CANCEL)
+    {
+        return;
+    }
+
+    // temp ????
+    wxArrayString fileList;
+    openFileDialog.GetPaths(fileList);
+    if (fileList.Count() == 1)
+    {
+        PushStatusText("1 file");
+    }
+    else
+    {
+        wxString filesNumber = wxString::Format(wxT("%d files"), (int)fileList.Count());
+        PushStatusText(filesNumber);
+    }
+    wxString displayList = "";
+    std::for_each(fileList.begin(), fileList.end(), [&displayList](const wxString f) { displayList.Append(f);  displayList.Append("\n"); });
+    wxLogMessage(displayList);
+}
+
+// FIXME: crashes on exit
+// bool FrameMain::OnDropFiles(wxCoord x, wxCoord y, const wxArrayString &filenames)
+// {
+//     wxLogMessage("blah!");
+//     return true;
+// }
